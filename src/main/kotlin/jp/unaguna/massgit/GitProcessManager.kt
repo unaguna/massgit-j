9b5e2fdfeed45e5769f4a3b5297fe2cb8b4c4d20
K-izumi <k.izumi.ysk@gmail.com>
2025-11-29 16:18:02 +0900

    🚧 loop by repos

--- src/main/kotlin/jp/unaguna/massgit/GitProcessManager.kt
+++ src/main/kotlin/jp/unaguna/massgit/GitProcessManager.kt
@@ -1,41 +1,61 @@
 package jp.unaguna.massgit
 
+import jp.unaguna.massgit.configfile.Repo
 import jp.unaguna.massgit.printfilter.LineHeadFilter
 import jp.unaguna.massgit.printmanager.PrintManagerThrough
+import java.nio.file.Path
 import kotlin.concurrent.thread
+import kotlin.io.path.Path
 
 class GitProcessManager(
     private val gitSubCommand: String,
     private val gitSubCommandArgs: List<String>,
+    private val repoDirectories: List<String>,
 ) {
-    fun run() {
-        val args = mutableListOf("git", gitSubCommand).apply {
-            addAll(gitSubCommandArgs)
-        }
-        val processBuilder = ProcessBuilder(args).apply {
-            redirectErrorStream(false)
-        }
-        val process = processBuilder.start()
+    fun run(massgitBaseDir: Path? = null) {
+        require(repoDirectories.isNotEmpty())
 
-        val processController = ProcessController(
-            process = process,
-            printManager = PrintManagerThrough(
-                LineHeadFilter("ping: ")
-            ),
-        )
+        val threads = repoDirectories.map { dirname ->
+            val args = mutableListOf("git", "-C", dirname, gitSubCommand).apply {
+                addAll(gitSubCommandArgs)
+            }
+            val processBuilder = ProcessBuilder(args).apply {
+                redirectErrorStream(false)
+                if (massgitBaseDir != null) {
+                    directory(massgitBaseDir.toFile())
+                }
+            }
+            val process = processBuilder.start()
 
-        val thread = thread {
-            processController.readOutput()
+            val processController = ProcessController(
+                process = process,
+                printManager = PrintManagerThrough(
+                    LineHeadFilter("$dirname: ")
+                ),
+            )
+
+            thread {
+                processController.readOutput()
+            }
         }
-        thread.join()
+        threads.forEach { it.join() }
     }
 
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val args = MainArgs.of(listOf("-x", "diff"))
-            GitProcessManager(args.subCommand!!, args.subOptions).run()
+            val massgitBaseDir = System.getProperty("jp.unaguna.massgit.projectDir")?.let { Path(it) }
+                ?: Path("").toAbsolutePath()
+            val repos = Repo.loadFromFile(massgitBaseDir.resolve(".massgit").resolve("repos.json"))
+            val repoDirectories = repos.map { it.dirname }
+
+            val args = MainArgs.of(listOf("-x", "grep", "gradle"))
+            GitProcessManager(
+                args.subCommand!!,
+                args.subOptions,
+                repoDirectories = repoDirectories,
+            ).run(massgitBaseDir)
         }
     }
 }
