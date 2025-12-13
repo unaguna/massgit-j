cfeb1d231cb35f7cab8e702e23cda4e76c1dc20b
K-izumi <k.izumi.ysk@gmail.com>
2025-12-13 10:33:30 +0900

    🔊 error message for unknown option

--- src/main/kotlin/jp/unaguna/massgit/MainArgs.kt
+++ src/main/kotlin/jp/unaguna/massgit/MainArgs.kt
@@ -1,5 +1,7 @@
 package jp.unaguna.massgit
 
+import jp.unaguna.massgit.exception.MassgitException
+
 data class MainArgs(
     val mainOptions: Options,
     val subCommand: String?,
@@ -69,7 +71,7 @@ data class MainArgs(
             val optionValue = optionParts.getOrNull(1)
 
             val optionDef = mainOptionDef.getOrElse(optionName) {
-                throw IllegalArgumentException("Unknown option: $optionName")
+                throw UnknownOptionException(optionName)
             }
 
             val option = addOption(optionDef)
@@ -134,3 +136,5 @@ data class MainArgs(
         }
     }
 }
+
+private class UnknownOptionException(unknownOption: String) : MassgitException("Unknown option: $unknownOption")
