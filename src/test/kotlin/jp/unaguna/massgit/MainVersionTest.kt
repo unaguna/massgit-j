66171f93588886e4dfd0695e2bd9e4e5b3959438
K-izumi <k.izumi.ysk@gmail.com>
2025-12-20 13:28:29 +0900

    ✅ --version

--- src/test/kotlin/jp/unaguna/massgit/MainVersionTest.kt
+++ src/test/kotlin/jp/unaguna/massgit/MainVersionTest.kt
@@ -9,11 +9,17 @@ class MainVersionTest {
         val mainArgs = MainArgs.of(listOf("--version"))
 
         val actualStdout = trapStdout {
-            Main().run(mainArgs, reposInj = emptyList())
+            Main().run(mainArgs, reposInj = emptyList(), gitProcessManagerFactoryInj = DummyProcessManagerFactory)
         }
 
         assert(actualStdout.startsWith("massgit on java"))
         assert(actualStdout.count { it == '\n' } == 1)
         assert(actualStdout.endsWith("\n"))
+    }
+
+    private object DummyProcessManagerFactory : GitProcessManagerFactory {
+        override fun create(): GitProcessManager {
+            error("must not be called")
+        }
     }
 }
